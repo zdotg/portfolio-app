4556965b126caf58736a98ed9e43e01e9f732ee5
Zach Gallman <zach.ecab@gmail.com>
2023-06-17 11:48:00 -0400

    Fixing errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,10 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <script
-        src="https://kit.fontawesome.com/f38f31d1bf.js"
-        crossorigin="anonymous"
-      ></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
